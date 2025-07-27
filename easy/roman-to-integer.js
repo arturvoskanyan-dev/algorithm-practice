c0654fdd9cb26cb2dcdba1f3c1be30182c33e1f1
arturvoskanyan-dev <arturvoskanyan.dev@gmail.com>
2025-07-27 16:39:25 +0400

    feat: solve Roman To Integer

---
+++ easy/roman-to-integer.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/roman-to-integer/description/
+
+var romanToInt = function (s) {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    let total = 0;
+
+    for(let i = 0; i < s.length; i++) {
+        let current = romanMap[s[i]];
+        let next = romanMap[s[i + 1]];
+
+        next > current ? total -= current : total += current;
+    }
+
+    return total
+}
+
+console.log(romanToInt("VIII"));
+console.log(romanToInt("LVIII"));
+console.log(romanToInt("MCMXCIV"));
