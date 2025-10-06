1893f96c8b6b44ebc17fbe8105b542f1fc09e03f
arturvoskanyan-dev <arturvoskanyan.dev@gmail.com>
2025-10-06 19:54:11 +0400

    solve longest common prefix problem

---
+++ easy/longest-common-prefix.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/longest-common-prefix/description/
+// Difficulty: Easy
+
+
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) return "";
+
+    let prefix = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1);
+            if (prefix === "") return "";
+        }
+    }
+
+    return prefix;
+};
+
+// console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+// console.log(longestCommonPrefix(["asd", "bcd", "edc"]));
