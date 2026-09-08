c14a14e4df644e3cb2e590a360b91afbdb983b1b
Artur <arturvoskanyan.dev@gmail.com>
2026-09-08 11:36:58 +0400

    add binary search problem

---
+++ easy/704-binary-search.js
@@ -0,0 +1,22 @@
+//leetcode.com/problems/binary-search/description/
+
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return -1;
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9));
