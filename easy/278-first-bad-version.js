2121147b8bf09da70460c41aff83793542f437be
Artur <arturvoskanyan.dev@gmail.com>
2026-09-08 15:21:49 +0400

    feat: add first bad version problem solution

---
+++ easy/278-first-bad-version.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/first-bad-version/
+
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    let left = 1;
+    let right = n;
+
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+
+      if (isBadVersion(mid)) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    return left;
+  };
+};
